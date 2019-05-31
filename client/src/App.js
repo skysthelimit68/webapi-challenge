b55b58eedbd669b9872949fdda3981430aeb0640
Ming Liu <mliu@macpro.home>
2019-05-31 14:17:38 -0400

    setup react app, add helper function to get all projects in projectModel.js, add get request route to retrieve all projects

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+class Projects extends React.Component {
+    componentDidMount() {
+        axios
+        
+    }
+    render() {
+
+    }
+}
+
+export default Projects;
