a107eeb8e1183b9bec668c0991279062d3e77f77
Ming Liu <mliu@macpro.home>
2019-05-31 12:56:02 -0400

    create middleware for action routers, post action and validate

--- routes/actionRouter.js
+++ routes/actionRouter.js
@@ -9,8 +9,35 @@ router.get("/:id", validateActionId, (req, res) => {
     res.status(200).json(req.body.action);
 })
 
+router.post("/", validateProjectId, validateAction, (req, res) => {
+    const newAction = { project_id : req.body.project_id, description : req.body.description, notes : req.body.notes }
+    actionDB.insert(newAction)
+    .then( response => {
+        res.status(201).json(response);
+    }) 
+    .catch( error => {
+        res.status(500).json({message: "error occured when trying to add an action"})
+    })
+})
+
 
 //middleware
+//check and see if a project exist
+function validateProjectId(req, res, next) {
+    projectDB.get(req.body.project_id) 
+    .then( project => {
+        if(project) {
+            req.project = project;
+            next();
+        } else {
+            res.status(404).json({message: "project not found"})
+        }
+    })
+    .catch (error => {
+        res.status(500).json({message: "something went wrong"})
+    })
+}
+
 //validate action id exist
 function validateActionId(req, res, next) {
     actionDB.get(req.params.id)
@@ -25,6 +52,15 @@ function validateActionId(req, res, next) {
     .catch( error => {
         res.status(500).json({message: "error occured when searching for action"})
     })
+}
+
+//validate project description and notes are in the new action post request body
+function validateAction(req, res, next) {
+    if(req.body.description && req.body.notes) {
+        next();
+    } else {
+        res.status(400).json({message: "please provide a description for this action"})
+    }
 }
 
 module.exports = router;
